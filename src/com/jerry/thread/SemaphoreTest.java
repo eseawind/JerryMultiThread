1fa642e0c5524050c4e8c9714cf2fca5dc9e25e5
JerryWang <jerry002@126.com>
2014-10-28 23:30:06 +0800

    Semaphore同步工具

---
+++ src/com/jerry/thread/SemaphoreTest.java
@@ -0,0 +1,48 @@
+package com.jerry.thread;
+
+import java.util.concurrent.ExecutorCompletionService;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Semaphore;
+
+/**
+ * Semaphore同步工具
+ * @author Jerry Wang
+ *
+ */
+public class SemaphoreTest {
+	public static void main(String[] args) {
+		ExecutorService service =  Executors.newCachedThreadPool();
+		final Semaphore semaphore = new Semaphore(3);
+		for(int i = 0; i < 10; i++) {
+			Runnable runable = new Runnable() {
+
+				@Override
+				public void run() {
+					try {
+						semaphore.acquire();
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
+					
+					System.out.println("线程" + Thread.currentThread().getName() + "进入，"
+						+	"当前已有" + (3 - semaphore.availablePermits() + "个并发"));
+					
+					try {
+						Thread.sleep((long) (Math.random() * 1000));
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
+					System.out.println("线程" + Thread.currentThread().getName() + "即将离开");
+					
+					semaphore.release();
+					
+					System.out.println("线程" + Thread.currentThread().getName() + "进入，"
+							+	"当前已有" + (3 - semaphore.availablePermits() + "个并发"));
+					
+				}
+			};
+			service.execute(runable);
+		}
+	}
+}
